18e398d8618046bef5efa11b83c490323e9a1166
saurabh <saurabhdeshpande49@gmail.com>
2016-05-25 23:56:23 +0530

    added the required functions for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object 
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minvrs <- NULL
+  set <- function(y) {
+    x <<- y
+    minvrs <<- NULL
+  }
+  get <- function() x
+  setminvrs <- function(inverse) minvrs <<- inverse
+  getminvrs <- function() minvrs
+  list(set = set, get = get,
+       setminvrs = setminvrs,
+       getminvrs = getminvrs)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  minvrs <- x$getminvrs()
+  if(!is.null(minvrs) && 
+     (is.matrix(minvrs) && is.matrix(solve(x$get())) && dim(minvrs) == dim(solve(x$get())) && all(minvrs == solve(x$get())))){
+    message("getting cached data")
+    return(minvrs)
+  }
+  
+  data <- x$get()
+  minvrs <- solve(data,...)
+  x$setminvrs(minvrs)
+  minvrs
 }
